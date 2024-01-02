89488537b014f9eee65f165e06a40b3895fdc918
cantuananh <tuananhthemen391997@gmail.com>
2024-01-02 15:16:29 +0700

    create user and encode password when create user

--- src/main/java/com/shopme/admin/controller/user/UserController.java
+++ src/main/java/com/shopme/admin/controller/user/UserController.java
@@ -1,22 +1,50 @@
 package com.shopme.admin.controller.user;
 
+import com.shopme.admin.model.Role;
+import com.shopme.admin.model.User;
 import com.shopme.admin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/shopme/admin")
+@RequestMapping("/shopme/admin/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/users")
+    @RequestMapping()
     public String getAllUsers(Model model) {
         model.addAttribute("listUsers", userService.listAll());
 
-        return "/admin/users";
+        return "/admin/user/users";
+    }
+
+    @RequestMapping("/new")
+    public String newUser(Model model) {
+        List<Role> listRoles = userService.listRoles();
+        User user = new User();
+        user.setEnabled(true);
+        model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
+
+        return "/admin/user/user_form";
+    }
+
+    @PostMapping("/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        System.out.println(user);
+
+        userService.save(user);
+        redirectAttributes.addFlashAttribute("message", "Create user successfully");
+
+
+        return "redirect:/shopme/admin/users";
     }
 
 
--- src/main/java/com/shopme/admin/service/UserService.java
+++ src/main/java/com/shopme/admin/service/UserService.java
@@ -1,8 +1,12 @@
 package com.shopme.admin.service;
 
+import com.shopme.admin.model.Role;
 import com.shopme.admin.model.User;
+import com.shopme.admin.repository.RoleRepository;
 import com.shopme.admin.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,7 +16,32 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<User> listAll() {
         return (List<User>) userRepository.findAll();
+    }
+
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        encodePassword(user);
+        userRepository.save(user);
+    }
+
+    private void encodePassword(User user) {
+        String encodePassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodePassword);
+
     }
 }
