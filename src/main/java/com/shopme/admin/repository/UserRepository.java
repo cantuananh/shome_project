137a81dc8081e73b3816267c8be8d849360976cd
cantuananh <tuananhthemen391997@gmail.com>
2024-01-05 11:13:22 +0700

    extent PagingAndSortingRepository to paginate list user

--- src/main/java/com/shopme/admin/repository/UserRepository.java
+++ src/main/java/com/shopme/admin/repository/UserRepository.java
@@ -4,13 +4,12 @@ import com.shopme.admin.model.User;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Transactional
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
     @Query("SELECT u from User u where u.email = :email")
     public User getUserByEmail(@Param("email") String email);
 
