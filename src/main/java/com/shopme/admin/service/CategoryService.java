3eadb34ae8a9699fa3914c927d8b173b523ae6cc
cantuananh <tuananhthemen391997@gmail.com>
2024-01-10 01:30:06 +0700

    code list category in hierarchical in name clumn

--- src/main/java/com/shopme/admin/service/CategoryService.java
+++ src/main/java/com/shopme/admin/service/CategoryService.java
@@ -15,8 +15,46 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     public List<Category> listAll() {
-        return (List<Category>) categoryRepository.findAll();
+        List<Category> rootCategories = categoryRepository.findRootCategories();
+
+        return listHierarchicalCategories(rootCategories);
     }
+
+    public List<Category> listHierarchicalCategories(List<Category> rootCategories) {
+        List<Category> hierarchicalCategories = new ArrayList<>();
+
+        for (Category rootCategory: rootCategories) {
+            hierarchicalCategories.add(Category.copyFull(rootCategory));
+
+            Set<Category> children = rootCategory.getChildren();
+            for (Category subCategory: children) {
+                String name = "--" + subCategory.getName();
+                hierarchicalCategories.add(Category.copyFull(subCategory, name));
+                listSubHierarchicalCategories(hierarchicalCategories, subCategory, 1);
+            }
+        }
+
+        return hierarchicalCategories;
+    }
+
+    public void listSubHierarchicalCategories(List<Category> hierarchicalCategories, Category parent, int subLevel) {
+        Set<Category> children = parent.getChildren();
+        int newSubLevel = subLevel + 1;
+
+        for (Category subCategory: children) {
+            String name = "";
+            for (int i = 0; i < newSubLevel; i++) {
+                name += "--";
+            }
+
+            name += subCategory.getName();
+
+            hierarchicalCategories.add(Category.copyFull(subCategory, name));
+
+            listSubHierarchicalCategories(hierarchicalCategories, subCategory, newSubLevel);
+        }
+    }
+
     public List<Category> listCategoriesUsedInForm() {
         List<Category> categoriesUseInForm = new ArrayList<>();
         Iterable<Category> categoriesInDB = categoryRepository.findAll();
@@ -24,14 +62,14 @@ public class CategoryService {
         for (Category category : categoriesInDB) {
             if (category.getParent() == null) {
                 categoriesUseInForm.add(Category.copyIdAndName(category.getId(), category.getName()));
-                listChildren(categoriesUseInForm, category, 1);
+                listSubCategoriesUsedInForm(categoriesUseInForm, category, 1);
             }
         }
 
         return categoriesUseInForm;
     }
 
-    public void listChildren(List<Category> categoriesUseInForm, Category parent, int subLevel) {
+    public void listSubCategoriesUsedInForm(List<Category> categoriesUseInForm, Category parent, int subLevel) {
         int newSubLevel = subLevel + 1;
         Set<Category> children = parent.getChildren();
 
@@ -43,7 +81,7 @@ public class CategoryService {
             name += subCategory.getName();
 
             categoriesUseInForm.add(Category.copyIdAndName(subCategory.getId(), name));
-            listChildren(categoriesUseInForm, subCategory, newSubLevel);
+            listSubCategoriesUsedInForm(categoriesUseInForm, subCategory, newSubLevel);
         }
     }
 
