ccc2d253198b4d06de21c089d2f4b83e822086ad
cantuananh <tuananhthemen391997@gmail.com>
2024-01-10 11:44:04 +0700

    update

--- src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -1,11 +1,9 @@
 package com.shopme.admin.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,8 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-@ComponentScan(basePackages = "com.shopme.admin")
 public class WebSecurityConfig {
     @Bean
     UserDetailsService userDetailsService() {
