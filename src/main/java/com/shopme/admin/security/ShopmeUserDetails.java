896ad4cd5efacf4c0aabc80f9238eb6c20142387
cantuananh <tuananhthemen391997@gmail.com>
2024-01-29 14:09:43 +0700

    code update accoutn details

--- src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
+++ src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
@@ -63,6 +63,14 @@ public class ShopmeUserDetails implements UserDetails {
     }
 
     public String getFullName(){
-        return this.user.getFirstName() + this.user.getLastName();
+        return this.user.getFirstName() + " " + this.user.getLastName();
+    }
+
+    public void setFirstName(String firstName) {
+        this.user.setFirstName(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        this.user.setLastName(lastName);
     }
 }
